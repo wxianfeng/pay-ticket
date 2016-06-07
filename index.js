5cd0787c658d9e4caa707696bb586015c20c2bd4
wxianfeng <wang.fl1429@gmail.com>
2016-06-07 22:54:03 +0800

    email insert ok

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var mysql = require('mysql');
+var fs = require('fs');
+var dateFormat = require('dateformat');
 
 var app = express();
 
@@ -18,20 +21,18 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+var dbContent = fs.readFileSync(__dirname + "/database.json", "utf8");
+var databaseCfg = JSON.parse(dbContent);
+var dev = databaseCfg.development;
 
+var connection = mysql.createConnection({
+  host     : dev.host,
+  user     : dev.user,
+  password : dev.password,
+  database: dev.database
+});
 
-// var dbContent = fs.readFileSync(__dirname + "/database.json", "utf8");
-// var databaseCfg = JSON.parse(dbContent);
-// var dev = databaseCfg.development;
-
-// var connection = mysql.createConnection({
-//   host     : dev.host,
-//   user     : dev.user,
-//   password : dev.password,
-//   database: dev.database
-// });
-
-// connection.connect();
+connection.connect();
 
 // development only
 if ('development' == app.get('env')) {
@@ -40,8 +41,29 @@ if ('development' == app.get('env')) {
 
 // save email
 app.post("/save-email", function(req, res){
-  console.log("==============");
-  res.send({ code: 0 });
+  var email = req.param('email');
+  connection.query("select 1 from users where email = ?", email, function(err, result){
+    console.log(err);
+    console.log(result);
+    console.log("-----------------------");
+
+    if (result.length == 0) { // email not exist
+      var date = new Date();
+      var date = dateFormat(date, "yyyy-mm-dd h:MM:ss");
+      console.log(date);
+      var sql = "insert into users(email, created_at, updated_at) values(\"" + email + "\", \""+  date +"\", \""+ date +"\")";
+      console.log(sql);
+      connection.query(sql, {}, function(err, result){
+        console.log(err);
+        console.log(result);
+      });
+    }
+
+    res.send({ code: 0 });
+    
+  });
+
+  // res.send({ code: 1, msg: "save email fail" });
 });
 
 http.createServer(app).listen(app.get('port'), function(){
