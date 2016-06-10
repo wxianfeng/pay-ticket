9256d2f7207163b905c37d76fc6b086a9eb6ee28
wxianfeng <wang.fl1429@gmail.com>
2016-06-10 12:17:11 +0800

    second email

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-// save email
+// save email and send first email
 app.post("/save-email", function(req, res){
   var email = req.param('email');
   connection.query("select 1 from users where email = ?", email, function(err, result){
@@ -80,18 +80,18 @@ app.post("/save-email", function(req, res){
         "The event will be consist of 3 segments within one week, which are Ethereum DevCon2, Demo Day and 2nd Global Blockchain Summit. Different types of tickets are provided for these segments. The purpose of this E-mail is to connect you with a payment system which allows you to pay for the tickets via Bitcoin or Ether. Please click one of the links below according to the ticket of your choosing, and a payment address for Ether or Bitcoin will be provided. Once the payment is confirmed (1 confirmation for Bitcoin, 10 confirmations for Ether), a coupon code will be provided to this E-mail address which can be used to claim the ticket on our event page on the Event Dove website.<br/>",
         "<b>Pay by Bitcoin</b>",
         "<b>Ticket for DevCon2</b>",
-        "https://localhost:3000/verify?token="+ token +"&category=bitcoin&ticket_category=2",
+        "http://localhost:3000/verify?token="+ token +"&category=bitcoin&ticket_category=2",
         "<b>Ticket for Demo Day and 2nd Global Blockchain Summit Ticket</b>",
-        "https://localhost:3000/verify?token="+ token +"&category=bitcoin&ticket_category=3",
+        "http://localhost:3000/verify?token="+ token +"&category=bitcoin&ticket_category=3",
         "<b>Ticket for the Whole Week</b>",
-        "https://localhost:3000/verify?token=" + token + "&category=bitcoin&ticket_category=1 <br/>",
+        "http://localhost:3000/verify?token=" + token + "&category=bitcoin&ticket_category=1 <br/>",
         "<b>Pay by Ether</b>",
         "<b>Ticket for DevCon2</b>",
-        "https://localhost:3000/verify?token=" + token + "&category=ether&ticket_category=2",
+        "http://localhost:3000/verify?token=" + token + "&category=ether&ticket_category=2",
         "<b>Ticket for Demo Day and 2nd Global Blockchain Summit</b>",
-        "https://localhost:3000/verify?token=" + token + "&category=ether&ticket_category=3",
+        "http://localhost:3000/verify?token=" + token + "&category=ether&ticket_category=3",
         "<b>Ticket for the Whole Week</b>",
-        "https://localhost:3000/verify?token=" + token + "&category=ether&ticket_category=1",
+        "http://localhost:3000/verify?token=" + token + "&category=ether&ticket_category=1 <br/>",
         "(This E-mail is sent by an automatic system. Please do not reply directly. )"
         ].join("<br/>");
 
@@ -117,6 +117,58 @@ app.post("/save-email", function(req, res){
   });
 
   // res.send({ code: 1, msg: "save email fail" });
+});
+
+// verify token and send second email
+app.get("/verify", function(req, res){
+  var token = req.param("token");
+
+  console.log(token);
+
+  if (undefined == token)
+    token = "";
+
+  connection.query("select 1 from users where token = ?", token, function(err, result){
+    console.log(err);
+    console.log(result);
+    if (result.length == 0) {
+      res.send("verify fail!");
+      return
+    }
+
+    var content = [
+      "Dear Guests,",
+      "You’ve ordered Ticket Name and choosed to pay by ______ _ , ",
+      "the payment address is __________________________ , the amount should be _____. You can double check the payment information by entering your E-mail at https://............................. .",
+      "Please finish the payment in 12 hours. Once the payment is confirmed (1 confirmation for Bitcoin, 10 confirmations for Ether), a coupon code will be provided to this E-mail address which can be used to claim the ticket on our event page on the Event Dove website.<br/>",
+
+      "(This E-mail is sent by an automatic system. Please do not reply directly. )"
+    ].join("<br/>");
+
+    var mailOptions = {
+      from: '"Pay-Ticket" <wxianfeng@wxianfeng.com>', // sender address
+      to: '522096432@qq.com', // list of receivers
+      subject: 'Please finish the payment for the ticket of Shanghai Blockchain Week.', // Subject line
+      html: content
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      return console.log(error);
+    }
+      console.log('Message sent: ' + info.response);
+    });
+
+    var html_content = [
+      "Dear Guests,",
+      "You’ve ordered Ticket Name and choosed to pay by ______ _ , ",
+      "the payment address is __________________________ , the amount should be _____. You can double check the payment information by entering your E-mail at https://............................. .",
+      "Please finish the payment in 12 hours. Once the payment is confirmed (1 confirmation for Bitcoin, 10 confirmations for Ether), a coupon code will be provided to this E-mail address which can be used to claim the ticket on our event page on the Event Dove website.",
+    ].join("<br/>");
+
+    res.send(html_content);
+  });
+
 });
 
 http.createServer(app).listen(app.get('port'), function(){
