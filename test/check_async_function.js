531b4da4f598c1916672f5b02d7885988eeb3623
wxianfeng <wang.fl1429@gmail.com>
2016-07-18 00:24:17 +0800

    fix async for, and slepp 10 seconds for request

---
+++ test/check_async_function.js
@@ -0,0 +1,41 @@
+var request = require("request");
+var sleep = require('sleep');
+
+function setI(i) {
+  // sleep(1);
+
+  console.log(i);
+}
+
+for (var i = 0; i < 10; i++) {
+  // console.log(i);
+
+  // sleep.sleep(1);
+
+  // console.log(i);
+
+  request("https://blockchain.info/q/addressbalance/1BnoTLVc9K2SEQe6USUvxNzJN2dCotoyT7?confirmations=1", function (error, response, body) {
+    // console.log(body);
+
+    setI("for: " + i);
+  })
+}
+
+// console.log("------------------------------------");
+
+var arr = [1,2,3,4,5,6,7,8,9,10];
+
+  // console.log(i);
+
+arr.forEach(function(i) {
+  // sleep.sleep(1);
+
+  // console.log(i);
+
+  request("https://blockchain.info/q/addressbalance/1BnoTLVc9K2SEQe6USUvxNzJN2dCotoyT7?confirmations=1", function (error, response, body) {
+    // console.log(body);
+
+    setI("forEach: " + i);
+  })
+})
+
