7f3b313098f503d5fb248c1682bdc2b2ab19e14e
wxianfeng <wang.fl1429@gmail.com>
2016-07-19 02:06:58 +0800

    fix aysnc select code

---
+++ test/select_code.js
@@ -0,0 +1,58 @@
+var mysql = require('mysql');
+var fs = require('fs');
+var sleep = require('sleep');
+var dbContent = fs.readFileSync(__dirname + "/database.json", "utf8");
+var databaseCfg = JSON.parse(dbContent);
+var dev = databaseCfg.development;
+
+function setCodeUsed(id) {
+  var connection2 = mysql.createConnection({
+    host     : dev.host,
+    user     : dev.user,
+    password : dev.password,
+    database: dev.database
+  });
+
+  connection2.connect();
+  connection2.query("update codes set state = ? where id = "+ id +"", "used", function(err, result){
+    if (err)
+      console.log("update code state error: " +  err);
+
+    connection2.end();
+  })
+  
+}
+
+var arr = [1,2,3];
+
+  // console.log(i);
+
+// 输出 1 - 10
+arr.forEach(function(ele, i) {
+
+  
+
+  var connection9 = mysql.createConnection({
+    host     : dev.host,
+    user     : dev.user,
+    password : dev.password,
+    database: dev.database
+  });
+  connection9.connect();
+
+  connection9.query("select * from codes where state = ? and ticket_category = ? limit ? LOCK IN SHARE MODE", ["unused", 1, arr.length], function(err, result) {
+    // sleep.sleep(2);
+    // console.log(result);
+
+    console.log(i);
+    console.log(result[i].id);
+
+    setCodeUsed(result[i].id);
+
+    connection9.end();
+
+  });
+
+
+});
+
